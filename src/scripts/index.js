12617ad1a820e3acfd51225da29ef924ffca7acd
Julianb18 <julian141718@gmail.com>
2020-03-02 16:07:59 +0100

    html and css complete

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,31 @@
 import "../styles/main.scss";
 // Import any additional modules you want to include below \/
 import "regenerator-runtime/runtime";
-
+import "bootstrap/dist/css/bootstrap.css";
 // \/ All of your javascript should go here \/
+
+const button = document.querySelector(`button`);
+const crypto = document.getElementById(`selectedCrypto`);
+const currency = document.getElementById(`selectedCurrency`);
+const baseUrl = `https://api.cryptonator.com/api/ticker`;
+const amountOfCrypto = document;
+
+button.addEventListener(`click`, e => {
+  e.preventDefault();
+  const url = `${baseUrl}/${crypto.value}-${currency.value}`;
+  console.log(url);
+  fetch(url)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error(`${res.status} ${res.statusText}`);
+      }
+    })
+    .then(data => {
+      console.log(data.ticker.price);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+});
