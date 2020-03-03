b6310d46a92ced9ae990883a76a594212341f0a4
Julianb18 <julian141718@gmail.com>
2020-03-03 13:17:45 +0100

    add word for abrv.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,16 +5,17 @@ import "regenerator-runtime/runtime";
 import "bootstrap/dist/css/bootstrap.css";
 // \/ All of your javascript should go here \/
 
-const button = document.querySelector(`button`);
+const convertButton = document.querySelector(`button`);
 const crypto = document.getElementById(`selectedCrypto`);
 const currency = document.getElementById(`selectedCurrency`);
 const baseUrl = `https://api.cryptonator.com/api/ticker`;
 let amountOfCrypto = document.querySelector(`.amountOfCrypto`);
 let costOfCrypto = document.querySelector(`.costOfCrypto`);
 
-button.addEventListener(`click`, e => {
+convertButton.addEventListener(`click`, e => {
   e.preventDefault();
   const url = `${baseUrl}/${crypto.value}-${currency.value}`;
+  console;
   console.log(url);
   fetch(url)
     .then(response => {
@@ -25,8 +26,10 @@ button.addEventListener(`click`, e => {
       }
     })
     .then(data => {
+      console.log(data);
       let price = data.ticker.price;
       let convert = amountOfCrypto.value * price;
+
       costOfCrypto.value = convert.toFixed(2);
 
       console.log(amountOfCrypto.value * price);
